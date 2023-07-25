5ff59dc340fd1484d7b219b38341d764c3ea1911
Sahil Iyer <sahiyer@deloitte.com.au>
2023-07-25 11:22:21 +1000

    Layout file for simon route.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,9 @@
 import Head from "next/head";
 import Link from "next/link";
-import styles from "./index.module.scss";
 
 export default function Index() {
   return (
     <main>
-      <Head>
-        <title>Minigames</title>
-      </Head>
-
       <h1>Minigames</h1>
       <h3>What would you like to play?</h3>
 
---
+++ src/app/simon/layout.tsx
@@ -0,0 +1,14 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Simon",
+  description: "A game of Simon Says",
+};
+
+export default function SimonLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return <section>{children}</section>;
+}
