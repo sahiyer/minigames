d1290efa5cab660041ee65061a9a1db9c5faadb4
Sahil Iyer <sahiyer@deloitte.com.au>
2023-07-26 09:48:45 +1000

    Added function to choose a pattern.

--- src/app/simon/page.tsx
+++ src/app/simon/page.tsx
@@ -6,12 +6,30 @@ export default function Simon() {
   const [gameState, setGameState] = useState(GameState.WaitingForStart);
   const [gameData, setGameData] = useState({
     currentLevel: 1,
+    bestLevel: -1,
+
+    pattern: new Array<number>(),
+    patternIndex: -1,
   });
+
+  const playPattern = () => {
+    setGameState(GameState.PlayingPattern);
+    setGameData((oldData) => ({
+      ...oldData,
+      pattern: chooseRandomPattern(gameData),
+      patternIndex: 0,
+    }));
+  };
 
   return (
     <>
       <h1>Simon</h1>
       <h3>{getStatus(gameState, gameData)}</h3>
+      <h5>Best: {gameData.bestLevel == -1 ? "None" : `Level ${gameData.bestLevel}`}</h5>
+
+      <button onClick={playPattern} disabled={gameState != GameState.WaitingForStart}>
+        Start
+      </button>
     </>
   );
 }
@@ -57,4 +75,15 @@ function getStatus(gameState: symbol, gameData: { currentLevel: number }) {
 
       return loseStatuses[Math.floor(Math.random() * loseStatuses.length)];
   }
+}
+
+function chooseRandomPattern(gameData: { currentLevel: number }) {
+  const pattern: number[] = [];
+
+  // Plus three because level 1 starts at a pattern of length 4.
+  for (let i = 0; i < gameData.currentLevel + 3; i++) {
+    pattern.push(Math.floor(Math.random() * 4));
+  }
+
+  return pattern;
 }
