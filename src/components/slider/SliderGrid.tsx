506ec4ea5e77925c06b2dfdb9a4edd1dbca7dcb0
Sahil Iyer <sahiyer@deloitte.com.au>
2023-07-28 10:35:10 +1000

    Remove one tile.

--- src/components/slider/SliderGrid.tsx
+++ src/components/slider/SliderGrid.tsx
@@ -1,22 +1,39 @@
 import SliderTile from "./SliderTile";
 import styles from "./SliderGrid.module.scss";
 
+interface Tile {
+    solvedRow: number;
+    solvedCol: number;
+}
+
 export default function SliderGrid() {
-    const tiles: number[][] = [];
-    for (let i = 0; i < 4; i++) {
-        tiles.push([0, 1, 2, 3]);
+    const tiles: Array<Array<Tile | null>> = [];
+    for (let row = 0; row < 4; row++) {
+        tiles.push([]);
+
+        for (let col = 0; col < 4; col++) {
+            tiles[tiles.length - 1].push({ solvedRow: row, solvedCol: col });
+        }
     }
+
+    // Randomly remove one tile.
+    const rowToRemoveFrom = Math.floor(Math.random() * 4);
+    const colToRemoveFrom = Math.floor(Math.random() * 4);
+    tiles[rowToRemoveFrom][colToRemoveFrom] = null;
 
     return (
         <div className={styles.sliderGrid}>
-            {tiles.map((row: number[], rowIndex: number) => {
+            {tiles.map((row: Array<Tile | null>, rowIndex: number) => {
                 return (
                     <>
-                        {row.map((col: number) => {
-                            return (
+                        {row.map((tile: Tile | null, colIndex) => {
+                            return tile == null ? (
+                                <></>
+                            ) : (
                                 <SliderTile
-                                    row={rowIndex}
-                                    col={col}
+                                    {...tile}
+                                    currentRow={rowIndex}
+                                    currentCol={colIndex}
                                 ></SliderTile>
                             );
                         })}
--- src/components/slider/SliderTile.tsx
+++ src/components/slider/SliderTile.tsx
@@ -1,10 +1,28 @@
 import clsx from "clsx";
 import styles from "./SliderTile.module.scss";
 
-export default function SliderTile({ row, col }: { row: number; col: number }) {
+export default function SliderTile({
+    solvedRow,
+    solvedCol,
+    currentRow,
+    currentCol,
+}: {
+    solvedRow: number;
+    solvedCol: number;
+    currentRow: number;
+    currentCol: number;
+}) {
     return (
         <div
-            className={clsx(styles.sliderTile, styles[`tile_${row}_${col}`])}
+            className={clsx(
+                styles.sliderTile,
+                styles[`tile_${solvedRow}_${solvedCol}`]
+            )}
+            style={{
+                // gridRow and gridColumn are 1-based.
+                gridRow: solvedRow + 1,
+                gridColumn: solvedCol + 1,
+            }}
         ></div>
     );
 }
